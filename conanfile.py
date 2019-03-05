7b4c5a1ca74e8a3c9e52ad0a3fbce6a845d422ac
Hreniuc Cristian-Alexandru <cristi@hreniuc.pw>
2019-03-05 17:42:48 +0200

    [Fix] Set option xcb to True for cairo dependency.

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ class CairoConan(ConanFile):
   author = "Hreniuc Cristian-Alexandru <cristi@hreniuc.pw>"
   license = "GNU LGPL 3"
   settings = "os", "arch", "compiler", "build_type"
-  default_options = "*:shared=False"
+  default_options = "*:shared=False", "cairo:xcb=True"
   generators = "cmake"
   chanel = "stable"
   author = "chreniuc"
